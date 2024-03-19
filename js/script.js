5bab372b327bc676c1be1c29f1d6c9702fb2a63d
Your Name <you@example.com>
2024-03-19 14:41:27 +0500

    clone

--- js/script.js
+++ js/script.js
@@ -1,22 +1,37 @@
 const swiper = new Swiper('.swiper', {
 
   loop: true,
-
-  autoplay:{
-    delay:2900,
-    disableOnInteraction:false,
-  },
+    autoplay:{
+        delay: 3000,
+        disableOnInteraction: false,
+    },
 
   pagination: {
-    clickkable:true,
-    el: '.swiper-pagination',
+      clickable: true,
+    el: '.swiper-pagination',   
   },
 
-
+ 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 
- 
+
 });
+
+const bars = document.querySelector('.fa-bars'),
+  navigation = document.querySelector('.navigation');
+
+
+  bars.addEventListener('click', () =>{
+    if(navigation.classList.contains('long')){
+      navigation.classList.add('short');
+      navigation.classList.remove('long');
+
+    }
+    else{
+      navigation.classList.add('long');
+      navigation.classList.remove('short');
+    }
+  })
